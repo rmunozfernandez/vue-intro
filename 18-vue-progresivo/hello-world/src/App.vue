1faded400891768f54741480c9dc3d61be248520
rmunozfernandez <rmunoz.it@gmail.com>
2023-12-11 20:30:12 -0300

    Vue progresivo

---
+++ 18-vue-progresivo/hello-world/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quibusdam beatae, repellendus incidunt voluptatum aspernatur obcaecati pariatur atque cum dolor! Obcaecati dolore cum eum reiciendis vero earum explicabo. Fugiat, deserunt.</p>
+  <div>{{ text }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+          text: 'Hola bro!'
+      }
+  }
+}
+</script>
+
+<style>
+p {
+  color: red;
+}
+</style>
